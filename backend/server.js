d58767d20f667dcd2b252b65d94f2f30e63e0f57
DAVI DEMETRIO DO CANTO <davi_canto@estudante.sesisenai.org.br>
2025-11-05 16:16:39 -0300

    feito user

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,21 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+app.use("/api/users", userRoutes);
 
-//Test route
-app.get('/', (req, res) => {
-    res.json({ message: 'API Blibioteca funcionando ' });
+app.get("/", (req, res) => {
+ res.json({ message: "API Biblioteca funcionando" });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
+ console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
